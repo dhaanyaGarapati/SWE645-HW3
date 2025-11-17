c6586c368726269e338342a4e09ec7e44f899c6e
Trinaya <75234659+Trinaya@users.noreply.github.com>
2025-11-17 14:35:48 -0500

    Update vite.config.js

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -1,7 +1,13 @@
+// Team Members:
+// Lasya Reddy Mekala (G01473683)
+// Supraja Naraharisetty(G01507868)
+// Trinaya Kodavati (G01506073)
+// Dhaanya S Garapati (G01512900)
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [
     react({
