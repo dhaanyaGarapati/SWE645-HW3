8be24f14bba699d1d4bddc7bead6c2ad5efa3271
Trinaya <75234659+Trinaya@users.noreply.github.com>
2025-11-17 14:15:28 -0500

    Add team member comments to models.py
    Added team member information as comments to the file.

--- models.py
+++ models.py
@@ -1,3 +1,9 @@
+# Team Members:
+# Lasya Reddy Mekala (G01473683)
+# Supraja Naraharisetty (G01507868)
+# Trinaya Kodavati (G01506073)
+# Dhaanya S Garapati (G01512900)
+
 from typing import List, Optional
 from sqlmodel import SQLModel, Field, Column, JSON
 from datetime import date
@@ -42,3 +48,4 @@ class SurveyUpdate(SQLModel):
     recommend: Optional[str] = None
     raffleNumbers: Optional[List[int]] = None
     comments: Optional[str] = None
+
