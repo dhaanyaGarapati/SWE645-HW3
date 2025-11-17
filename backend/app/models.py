1d0a61bb35fd9f553ca047e812e52e1557ee87fc
Trinaya <75234659+Trinaya@users.noreply.github.com>
2025-11-17 14:25:36 -0500

    Add team members' names to models.py
    Added team member information as comments.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,9 @@
+# Team Members:
+# Lasya Reddy Mekala (G01473683)
+# Supraja Naraharisetty (G01507868)
+# Trinaya Kodavati (G01506073)
+# Dhaanya S Garapati (G01512900)
+
 from typing import List, Optional
 from sqlmodel import SQLModel, Field, Column, JSON
 from datetime import date
